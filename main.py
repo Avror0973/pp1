dfc807d6976fe36d150634a920483ee6ee42a7f4
bambin <kozarakovca@gmail.com>
2025-01-26 17:25:01 +0300

    second phase achieved

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
-import uvicorn
 from fastapi import FastAPI
 from pydantic import EmailStr, BaseModel
 
+import uvicorn
+
+from items_views import router as items_router
+from users.views import router as users_router
+
 app = FastAPI()
+app.include_router(items_router)
+app.include_router(users_router)
 
-
-class CreateUser(BaseModel):
-    email: EmailStr
 
 
 @app.get("/")
@@ -24,13 +27,6 @@ def hello(name: str = "World"):
     }
 
 
-@app.post("/users/")
-def create_user(user: CreateUser):
-    return {
-        "message": "success",
-        "email": user.email
-            }
-
 
 @app.get("/calc/add/")
 def add(a: int, b: int):
@@ -38,33 +34,6 @@ def add(a: int, b: int):
         "a": a,
         "b": b,
         "result": a + b
-    }
-
-
-@app.get("/items/")
-def list_items():
-    return [
-        "item1",
-        "item2",
-    ]
-
-
-@app.get("/items/latest")
-def get_latest_item():
-    return {
-        "item": {
-            "id": 8,
-            "name": "latest",
-        }
-    }
-
-
-@app.get("/items/{item_id}")
-def get_item_by_id(item_id: int):
-    return {
-        "item":{
-            "id": item_id,
-        }
     }
 
 
