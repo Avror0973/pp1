0ab26f385ad379641b8177ecc85c25ed9952ae60
bambin <kozarakovca@gmail.com>
2025-01-31 15:59:00 +0300

    created DataBase5 completed

--- api_v1/products/crud.py
+++ api_v1/products/crud.py
@@ -33,14 +33,14 @@ async def create_product(session:AsyncSession, product_in:ProductCreate)-> Produ
 
 
 async def update_product(
-        session: AsyncSession,
-        product: Product,
-        product_update: ProductUpdate | ProductUpdatePartial,
-        partial: bool = False,
+    session: AsyncSession,
+    product: Product,
+    product_update: ProductUpdate | ProductUpdatePartial,
+    partial: bool = False,
 ) -> Product:
     for name, value in product_update.model_dump(exclude_unset=partial).items():
         setattr(product, name, value)
-    await session.close()
+    await session.commit()
     return product
 
 
